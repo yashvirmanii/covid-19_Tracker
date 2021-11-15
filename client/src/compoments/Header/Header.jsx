3eca12b44ce70f2572031050dc01b28d9be172fc
arpit <yashvirmani101@gmail.com>
2021-11-15 16:55:20 +0530

    Added Header component

--- client/src/compoments/Header/Header.jsx
+++ client/src/compoments/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from "react"
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+
+import useStyles from './styles';
 
 const Header = () => {
+    const classes = useStyles();
     return (
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' className={classes.title}>
+                    Covid-19 Tracker
+                </Typography>
+                <Box display="flex">
+                    <Typography variant='h6' className={classes.title}>
+                        Monitor Covid-19 impact
+                    </Typography>
+                    {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <InputBase placeholder="Search country" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+                    </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
