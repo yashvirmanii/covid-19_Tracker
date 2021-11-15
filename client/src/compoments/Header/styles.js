e8eee9c41a4e7e7eb7ca31c757aa4db191206dc9
arpit <yashvirmani101@gmail.com>
2021-11-15 19:30:18 +0530

    Implemented Google Maps

--- client/src/compoments/Header/styles.js
+++ client/src/compoments/Header/styles.js
@@ -17,9 +17,6 @@ export default makeStyles((theme) => ({
         width: '100%',
         [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
     },
-    searchIcon: {
-        padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
-    },
     inputRoot: {
         color: 'inherit',
     },
--- client/src/compoments/Map/Map.jsx
+++ client/src/compoments/Map/Map.jsx
@@ -1,8 +1,33 @@
 import React from "react"
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from "@material-ui/core"
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+
+import useStyle from './styles'
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const Map = () => {
+    const classes = useStyle();
+    const isMobile = useMediaQuery('(min-width:600px)')
+
+    const mapData = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyBL7aRxlsq3Z_vtVMak8UKHZeYU_PGxVLI" }}
+                defaultCenter={mapData.center}
+                defaultZoom={mapData.zoom}
+                margin={[50, 50, 50, 50]}
+            >
+            </GoogleMapReact>
+        </div>
     )
 }
 
